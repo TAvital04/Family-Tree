e99a5fb2ff9fd74752b11738a68fabfd6c8b6ccb
Tal Avital <tal.avital04@gmail.com>
2025-07-11 07:04:44 -0400

    family name change leak

--- controllers/familyController.js
+++ controllers/familyController.js
@@ -1,7 +1,7 @@
 import familyHandler from "../handlers/familyHandler.js";
 
 const getFamilies = async (req, res) => {
-    const families = await treeHandler.getAllFamilies(req.user._id);
+    const families = await familyHandler.getAllFamilies(req.user._id);
 
     res.send({
         ...families
